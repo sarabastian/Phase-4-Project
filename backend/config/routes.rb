6b79710aad057460801de9d1db12f64f6682ddc9
sarabastian <seb118@georgetown.edu>
2020-12-23 11:20:00 -0500

    some seed data

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :vans
+      resources :vans, only: [:show, :index]
       resources :van_images, only: [:show]
       resources :images, only: [:show, :index]
       resources :van_images, only: [:show, :index]
