641a167e6005530a96d9f8db834be4e981b2efd9
sarabastian <seb118@georgetown.edu>
2020-12-28 11:06:46 -0500

    carousel inside modal wip

--- frontend/src/Components/VanShow.js
+++ frontend/src/Components/VanShow.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, Carousel } from 'react-bootstrap';
 
 class VanShow extends React.Component {
 
@@ -10,17 +10,60 @@ class VanShow extends React.Component {
       <>
         <div
           className="van-show"
-          
+
         >
-    
+
         </div>
         <Modal show={this.props.show} onHide={this.props.closeModal}
-        dialogClassName="modal-90w"
-        aria-labelledby="example-custom-modal-styling-title">
-          <Modal.Header  closeButton>
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered>
+          <Modal.Header closeButton>
             <Modal.Title>{this.props.van.name}</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Includes: {this.props.van.interior_features}</Modal.Body>
+          <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={this.props.van.images[0]}
+                  alt = 'First Slide'
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={this.props.van.images[1]}
+                  alt='Second Slide'
+                />
+
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block 1-100"
+                  src={this.props.van.images[2]}
+                  alt = 'Third Slide'
+                />
+
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+
+          <Modal.Body>Includes: {this.props.van.interior_features}
+            
+
+
+          </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={this.props.closeModal}>Close</Button>
           </Modal.Footer>
