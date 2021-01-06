996d74af1e80fbf02cd1e2b4ba00ff5dca940e9e
sarabastian <seb118@georgetown.edu>
2021-01-06 14:27:36 -0500

    fixed some controllers

--- backend/app/controllers/api/v1/orders_controller.rb
+++ backend/app/controllers/api/v1/orders_controller.rb
@@ -2,22 +2,30 @@ class Api::V1::OrdersController < ApplicationController
 
     def index
         orders = Order.all
+        render json: orders
     end
 
     def show
         order = Order.find(params[:id])
+        render json: order
     end
 
     def create
         order = Order.new(order_params)
-        if @order.save render json:review
+        if @order.save render json: order
         end
     end
 
-    def delete
+    def destroy
         order = Order.find(params[:id])
-        order.delete
+        if order.user != current_user
+        else
+            order.destroy
+            render json: {msg: "Reservation was deleted.."}
+        end
     end
+
+    private
 
     def order_params
         params.require(:order).permit(:user_id, :van_id)
