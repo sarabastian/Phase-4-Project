2fcf3c3a10ac63f2914631293e22d517237ea97b
hugodelgado <hugo.delgado1@outlook.com>
2021-01-08 03:25:14 -0600

    ignore

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -64,5 +64,32 @@ class ApplicationController < ActionController::API
     #     token = request.headers['auth-key']
     #     payload = JWT.decode(token, 'Secret3', false)
     # end
+    def auth_header
+        request.headers['Authorization']
+      end
+    
+      def decoded_token
+        if auth_header
+          token = auth_header.split(' ')[1]
+          begin
+            JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+          rescue JWT::DecodeError
+            nil
+          end
+        end
+      end
+    
+      def current_user
+        if decoded_token
+          # decoded_token=> [{"user_id"=>2}, {"alg"=>"HS256"}]
+          # or nil if we can't decode the token
+          user_id = decoded_token[0]['user_id']
+          @user = User.find_by(id: user_id)
+        end
+      end
+    
+      def logged_in?
+        !!current_user
+      end
 
 end
